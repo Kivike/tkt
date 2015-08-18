3d5256e5289123844d59e5758c63efa5d41c0bf2
Roope Rajala <r_rajala@yahoo.com>
2015-08-18 17:05:18 +0300

    Assembler remake. Somewhat working at least.

---
+++ Assembler/src/Label.java
@@ -0,0 +1,13 @@
+/*
+ * Label is a named memory slot which contains a command
+ */
+public class Label {
+    // Label name
+    public String name;
+
+    // Label command
+    public String command;
+
+    // Labels memory slot
+    public Short memorySlot;
+}
